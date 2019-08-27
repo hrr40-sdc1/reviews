61677a3027a366cc93d7e892258082a1aa852fdf
SeanKier <seantkier@gmail.com>
2019-08-26 19:28:35 -0700

    weback working with react

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: __dirname + '/client/src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/client/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};

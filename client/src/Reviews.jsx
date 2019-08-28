dd654c81bf847624e35739db5a3af9c7206cb684
SeanKier <seantkier@gmail.com>
2019-08-28 10:25:32 -0700

    clean up Reviews

--- client/src/Reviews.jsx
+++ client/src/Reviews.jsx
@@ -9,41 +9,8 @@ class Reviews extends React.Component {
       expanded: false
     }
   }
-  expandedText() {
-    this.setState({
-        expanded: true
-    });
-  }
+ 
   render(){
-    // if (this.state.expanded === false && this.props.review.comment.length > 5) {
-    //   const message = this.props.review.comment.split('\n').map((paragraph, i) => {
-    //     return <p key={i}>{paragraph}</p>
-    //   });
-    //   const subMessage = message.slice(0, 5);
-    //   // console.log(subMessage);
-    //   return(
-    //     <div>
-    //       <div className="image">
-    //         <img src={this.props.review.userImage}></img>
-    //       </div>
-    //       <div className="username">
-    //         {this.props.review.userName}
-    //       </div>
-    //       <div className="date">
-    //         {moment(Date.parse(this.props.review.dateCreated)).format("MMMM YYYY")}
-    //       </div>
-    //       <div className="message">
-    //         <div>
-    //         {subMessage}
-    //         </div>
-    //         <div>
-
-    //         </div>
-
-    //       </div>
-    //     </div>
-    //   )
-    // }
 
     return(
       <div>
